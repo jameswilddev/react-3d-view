11d53437751a866492f0e97709d8545ba72f177a
jameswilddev <jameswilddev@outlook.com>
2022-02-17 21:38:49 +0000

    Fix class being abstract.

--- src/webgl/static-triangle-index-buffer.tsx
+++ src/webgl/static-triangle-index-buffer.tsx
@@ -4,7 +4,7 @@ import { TriangleIndexBuffer } from "./triangle-index-buffer";
 /**
  * A buffer of static element indices representing triangles, grouped into triangles.
  */
-export abstract class StaticTriangleIndexBuffer extends TriangleIndexBuffer {
+export class StaticTriangleIndexBuffer extends TriangleIndexBuffer {
   /**
    * Creates a new buffer of static triangle element indices.  All must be integers between 0 and 65535, inclusive.  Cannot be empty.
    * @param context The context under which the buffer will be created.
