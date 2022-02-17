053819018399012eb2317f44f547c8d8dc974fcb
jameswilddev <jameswilddev@outlook.com>
2022-02-17 19:03:12 +0000

    Throw when a context cannot be created.

--- src/webgl/context.tsx
+++ src/webgl/context.tsx
@@ -3,9 +3,9 @@
  */
 export class Context {
   /**
-   * The WebGL context, if any, otherwise null.  May be experiencing context loss.
+   * The WebGL context.  May be experiencing context loss.
    */
-  readonly _gl: null | WebGLRenderingContext;
+  readonly _gl: WebGLRenderingContext;
 
   /**
    * The number of times that the WebGL context has been restored.
@@ -17,20 +17,25 @@ export class Context {
    * @param canvas The canvas for which to create and manage a WebGL context.
    * @param options The options to use when creating the WebGL context.
    * @param onRefresh Executed when a refresh of the display is known to be necessary, e.g. when the context is restored or the element is resized.
+   * @throws When a WebGL context cannot be created.
    */
   constructor(
     canvas: HTMLCanvasElement,
     options: WebGLContextAttributes,
     onRefresh: () => void
   ) {
-    this._gl =
+    const gl =
       canvas.getContext(`webgl`, options) ??
       (canvas.getContext(
         `experimental-webgl`,
         options
       ) as null | WebGLRenderingContext);
 
-    if (this._gl !== null) {
+    if (gl === null) {
+      throw new Error(`Failed to create a WebGL context.`);
+    } else {
+      this._gl = gl;
+
       canvas.addEventListener(`webglcontextlost`, (event) => {
         event.preventDefault();
       });
