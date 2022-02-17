a1ad4342d38f9f18bf30c0474d2e4cdf26b0e61b
jameswilddev <jameswilddev@outlook.com>
2022-02-17 22:44:42 +0000

    Refactor type.

--- src/webgl/variable-set.tsx
+++ src/webgl/variable-set.tsx
@@ -1,4 +1,4 @@
-import type { ShaderPrimitive } from "./shader-primitive";
+import type { VariableSetItem } from "./variable-set-item";
 
 /**
  * Describes a set of variables, e.g. the attributes or uniforms fed into a shader program.
@@ -7,15 +7,5 @@ export type VariableSet = {
   /**
    * Maps the names of the variables to their type information.
    */
-  readonly [key: string]: {
-    /**
-     * The primitive type of the variable within the shader.
-     */
-    readonly shaderPrimitive: keyof ShaderPrimitive;
-
-    /**
-     * When 1, the variable is NOT an array.  When greater than 1, the variable is an array of the specified length.
-     */
-    readonly quantity: number;
-  };
+  readonly [key: string]: VariableSetItem;
 };
