6ba068417883cfd84d14d504ff3c6744aee573f7
jameswilddev <jameswilddev@outlook.com>
2022-02-17 19:01:54 +0000

    Add normalization flags.

--- src/webgl/attribute-set-item.tsx
+++ src/webgl/attribute-set-item.tsx
@@ -1,16 +1,79 @@
-import type { AttributePrimitive } from "./attribute-primitive";
-
 /**
  * Describes an attribute.
  */
-export type AttributeSetItem = {
-  /**
-   * The primitive type of the attribute within the array buffer.
-   */
-  readonly attributePrimitive: keyof AttributePrimitive;
+export type AttributeSetItem =
+  | {
+      /**
+       * The primitive type of the attribute within the array buffer.
+       */
+      readonly attributePrimitive: `u8`;
 
-  /**
-   * The primitive type of the attribute within the shader.
-   */
-  readonly shaderPrimitive: `float` | `vec2` | `vec3` | `vec4`;
-};
+      /**
+       * The primitive type of the attribute within the shader.
+       */
+      readonly shaderPrimitive: `float` | `vec2` | `vec3` | `vec4`;
+
+      /**
+       * When true, the value is normalized, meaning that 0 is 0 and 255 is 1.
+       */
+      readonly normalized: boolean;
+    }
+  | {
+      /**
+       * The primitive type of the attribute within the array buffer.
+       */
+      readonly attributePrimitive: `u16`;
+
+      /**
+       * The primitive type of the attribute within the shader.
+       */
+      readonly shaderPrimitive: `float` | `vec2` | `vec3` | `vec4`;
+
+      /**
+       * When true, the value is normalized, meaning that 0 is 0 and 65535 is 1.
+       */
+      readonly normalized: boolean;
+    }
+  | {
+      /**
+       * The primitive type of the attribute within the array buffer.
+       */
+      readonly attributePrimitive: `s8`;
+
+      /**
+       * The primitive type of the attribute within the shader.
+       */
+      readonly shaderPrimitive: `float` | `vec2` | `vec3` | `vec4`;
+
+      /**
+       * When true, the value is normalized, meaning that -128 is -1 and 127 is 1.
+       */
+      readonly normalized: boolean;
+    }
+  | {
+      /**
+       * The primitive type of the attribute within the array buffer.
+       */
+      readonly attributePrimitive: `s16`;
+
+      /**
+       * The primitive type of the attribute within the shader.
+       */
+      readonly shaderPrimitive: `float` | `vec2` | `vec3` | `vec4`;
+
+      /**
+       * When true, the value is normalized, meaning that -32768 is -1 and 32767 is 1.
+       */
+      readonly normalized: boolean;
+    }
+  | {
+      /**
+       * The primitive type of the attribute within the array buffer.
+       */
+      readonly attributePrimitive: `f32`;
+
+      /**
+       * The primitive type of the attribute within the shader.
+       */
+      readonly shaderPrimitive: `float` | `vec2` | `vec3` | `vec4`;
+    };
