3f7a6d4a4f1a056908ab701d861ce31bc89eb573
jameswilddev <jameswilddev@outlook.com>
2022-02-17 17:56:11 +0000

    Remove the quantity as it isn't possible to have an array attribute.

--- src/webgl/attribute-set-item.tsx
+++ src/webgl/attribute-set-item.tsx
@@ -14,9 +14,4 @@ export type AttributeSetItem = {
    * The primitive type of the attribute within the shader.
    */
   readonly shaderPrimitive: keyof ShaderPrimitive;
-
-  /**
-   * When 1, the attribute is NOT an array.  When greater than 1, the attribute is an array of the specified length.
-   */
-  readonly quantity: number;
 };
