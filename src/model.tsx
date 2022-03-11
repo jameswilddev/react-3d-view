f71dcaef5468f97e8e7f9226ec4f3fd74b684632
jameswilddev <jameswilddev@outlook.com>
2022-03-11 22:26:10 +0000

    Add missing comment.

--- src/model.tsx
+++ src/model.tsx
@@ -4,5 +4,8 @@ import type { Polygon } from ".";
  * A vertex-colored model which can be rendered in a viewport.
  */
 export type Model = {
+  /**
+   * The polygons of the model.
+   */
   readonly polygons: ReadonlyArray<Polygon>;
 };
